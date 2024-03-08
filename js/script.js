d363ff6c298b93aa78b005a81c1e65e89b57a063
LupeF <46634202+LupeF@users.noreply.github.com>
2024-03-08 14:46:20 -0800

    add styles and images for darkmode

--- js/script.js
+++ js/script.js
@@ -1,4 +1,5 @@
 const moon = document.getElementById('moon-container');
+const sun = document.getElementById('sun-icon');
 const moonIcon = document.getElementById('moon-icon');
 const backgroundColor = document.getElementById('night-color');
 const featuresColor = document.getElementById('features-container');
@@ -8,7 +9,9 @@ const syntaxSymbol= document.getElementById('syntax')
 const wand = document.getElementById('wand')
 const phone = document.getElementById('phone')
 
-moon.addEventListener('click', (e)=>{
+moonIcon.addEventListener('click', (e)=>{
+    moonIcon.style.display = 'none';
+    sun.style.display = 'block';
     backgroundColor.style.backgroundColor='#343A3D';
     featuresColor.style.backgroundColor='#3D4346';
     image.src = 'assets/hero-dark-mode.png';
@@ -16,5 +19,17 @@ moon.addEventListener('click', (e)=>{
     syntaxSymbol.src = 'assets/icon-syntax-gradient.png';
     wand.src = 'assets/icon-wand-gradient.png';
     phone.src = 'assets/icon-phone-gradient.png';
-    moonIcon.src ='assets/icon-sun-white.png';
+})
+
+sun.addEventListener('click', (e)=>{
+    sun.style.display = 'none';
+    moonIcon.style.display = 'block';
+    backgroundColor.style.backgroundColor='#F9F9FA';
+    featuresColor.style.backgroundColor='#E1E2E4';
+    image.src = 'assets/hero-light-mode.png';
+    lightningBolt.src = 'assets/icon-lightning-gray.png';
+    syntaxSymbol.src = 'assets/icon-syntax-gray.png';
+    wand.src = 'assets/icon-wand-gray.png';
+    phone.src = 'assets/icon-phone-gray.png';
+
 })
